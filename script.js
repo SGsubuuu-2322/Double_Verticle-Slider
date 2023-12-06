d5f826d6ff074537aa35f84d63451822d371c233
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-06 23:04:44 +0530

    Added some basic scripts to the project...

---
+++ script.js
@@ -0,0 +1,6 @@
+const sliderContainer = document.querySelector(".slider-container");
+const sliderLeft = document.querySelector(".left-slide");
+const sliderRight = document.querySelector(".right-slide");
+const buttonUp = document.querySelector(".up-button");
+const buttonDown = document.querySelector(".down-button");
+const slidesLength = sliderRight.querySelectorAll("div").length;
