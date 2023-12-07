7dc41239dc508dee314a02dbf80c312cbc49d7c7
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-07 21:21:59 +0530

    Added the script code for setting the style for left slide...

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const sliderRight = document.querySelector(".right-slide");
 const buttonUp = document.querySelector(".up-button");
 const buttonDown = document.querySelector(".down-button");
 const slidesLength = sliderRight.querySelectorAll("div").length;
+
+sliderLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
