325ff3e97c912eaa62e255aa2821378e327012e1
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-10 23:57:15 +0530

    Added some more scripts for right slider animation

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const buttonUp = document.querySelector(".up-button");
 const buttonDown = document.querySelector(".down-button");
 const slidesLength = sliderRight.querySelectorAll("div").length;
 
+let activeSlideIndex = 0;
+
 sliderLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 buttonUp.addEventListener("click", () => changeSlide("up"));
 buttonDown.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  }
+  sliderRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }PX)`;
+};
